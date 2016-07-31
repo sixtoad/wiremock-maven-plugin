892dd2d823512f5433ea637daa4132baf9cf48e0
cantolls <cantollas@theaa.ie>
2016-07-31 10:00:12 +0100

    Added parameters

--- src/main/java/wiremock/MyMojo.java
+++ src/main/java/wiremock/MyMojo.java
@@ -3,17 +3,30 @@ package wiremock;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
+import com.github.tomakehurst.wiremock.common.SingleRootFileSource;
+import com.github.tomakehurst.wiremock.standalone.CommandLineOptions;
 import com.github.tomakehurst.wiremock.standalone.WireMockServerRunner;
 
 /**
- * Goal which touches a timestamp file.
+ * Goal which startup wiremock
  */
 @Mojo(name = "run")
 public class MyMojo extends AbstractMojo {
-
+    
+    @Parameter( property = "wiremock.port", defaultValue = "9091" )
+    private String port;
+    
+    @Parameter( property = "wiremock.mapping", defaultValue = "target/save-mappings-files/" )
+    private String mapping;
+    
+    
     public void execute() throws MojoExecutionException {
         getLog().info("Starting wiremock.");
-        WireMockServerRunner.main("--port", "9091");
+        new WireMockServerRunner().run("--root-dir",mapping,"--port", port,"--verbose","--extensions", "com.opentable.extension.BodyTransformer");
+        while(true) {
+            
+        }
     }
 }
